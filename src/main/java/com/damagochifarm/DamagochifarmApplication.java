763505669a21ee8379a3db4d13f46d3f18c58be3
kyung <bamtori1029@gmail.com>
2024-11-08 20:51:07 +0900

    first commit

---
+++ src/main/java/com/damagochifarm/DamagochifarmApplication.java
@@ -0,0 +1,13 @@
+package com.damagochifarm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamagochifarmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamagochifarmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/damagochifarm/DamagochifarmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damagochifarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamagochifarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
